df5ce7b95f770e525387ebbdbd02873335d91aaf
Matt Javier <matthewrjavier@gmail.com>
2020-07-22 15:21:24 -0700

    changes to index title

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// character arrays
+const num = []
+const alpha = []
+const special = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', 
+                '*', '+', ',', '-', '.', '/', ':', ';', '<', '=',
+                '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{',
+                '|', '}', '~']
+
+function generatePassword() {
+  // use prompt() and confirm() to interact with user
+  
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword()
+  let passwordText = document.querySelector("#password")
 
-  passwordText.value = password;
-
+  passwordText.value = password
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
